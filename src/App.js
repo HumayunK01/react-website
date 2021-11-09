8c192f9f2588c0de2ee8051404766167a927160f
HumayunK01 <88980866+HumayunK01@users.noreply.github.com>
2021-11-09 17:44:14 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Navbar from './components/pages/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Wassup</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' />
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/pages/navbar.js
+++ src/components/pages/navbar.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { MdFingerprint } from 'react-icons/md'
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 function navbar() {
     return (
-        <div>
-            
+        <div className="navbar">
+            <div className="navbar-container container">
+                <Link to='/' className="navbar-logo">
+                    <MdFingerprint className="navbar-icon" />
+                    LAVISH
+                </Link>
+                <div className="menu-icon">
+                {click ? <FaTimes /> : <FaBars />}
+                </div>
+            </div> 
         </div>
     )
 }
